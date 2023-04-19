442ddbe634921b7582b89056b0a16a96e558301a
Mariana Hernandez <99154412+MarianaHernandezC@users.noreply.github.com>
2023-04-19 15:11:11 -0600

    Add files via upload

--- p1.asm
+++ p1.asm
@@ -1,40 +1,62 @@
-section .data
- msg db 'Ingrese una frase corta: ',0		;primera cadena
- len1 equ $-len1 				;tamaño de la primera cadena
- resultado db 'La cadena invertida es: ',0	;segunda cadena
- len2 equ $-len2 				;tamaño de la segunda cadena
+
+SECTION .DATA
+prompt db 'Ingrese una frase: ', 0
+output db 'La frase invertida es: ', 0
 
 section .bss
- buffer resb 30     ;espacio reservado  donde se guardara la frase
- len resd 1         ;espacio reservado para guardar el tamaño de la frase ingresada
+input resb 256
 
-section  .txt
- global _start
-_start:
- ;se imprime el mensaje en la terminal para ingresar la frase
- mov eax, 4
- mov ebx, 0
- mov ecx, msg
- mov edx, len1
- int 0x80
+SECTION .TEXT
+global _start
 
- ;se lee el mensaje o el texto ingresado por el usuario
- mov eax, 3
- mov ebx, 0
- mov ecx, buffer
- mov edx, 30
- int 0x80
+    _start:
+        BITS 32
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, prompt
+        mov edx, 18
+        int 0x80
+    
+        ; Leer entrada
+        mov eax, 3
+        mov ebx, 0
+        mov ecx, input
+        mov edx, 255
+        int 0x80
+    
+        ; Cadena de entrada alrevez
+        mov esi, input
+        mov edi, esi
+        add edi, edx
+        dec edi
+        mov ebx, edx
+        shr ebx, 1
+    
+    reverse_loop:
+        mov al, byte [esi]
+        mov dl, byte [edi]
+        mov byte [esi], dl
+        mov byte [edi], al
+        inc esi
+        dec edi
+        dec ebx
+        jnz reverse_loop
+    
 
- mov edi, ecx
- mov ecx, 0
- mov eax, 0
- call strlen
-strlen:
- ; Se agrega strlen para obtener el tamaño de la cadena
- cmp [edi],0
- jz function            ;salto a la función donde se guardara la frase invertida
- inc eax
- inc edi
- jmp strlen
- 
- 
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, output
+        mov edx, 20
+        int 0x80
+    
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, input
+        mov edx, 256
+        int 0x80
+    
+        ; Salir del programa
+        mov eax, 1
+        xor ebx, ebx
+        int 0x80
+    
